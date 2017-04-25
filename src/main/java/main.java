46cf33ada42cc23d9aa051b088e30062a04c593e
Marc Visser <M.Visser-6@student.tudelft.nl>
2017-04-25 12:47:01 +0200

    gfa file parser read headers

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,14 +1,19 @@
-import org.biojava.nbio.core.exceptions.CompoundNotFoundException;
-import org.biojava.nbio.core.sequence.DNASequence;
-
-/**
- * Created by marc on 25-4-17.
- */
-
+import java.io.File; import java.io.FileInputStream; import java.util.LinkedHashMap; import java.util.Map.Entry;
+import org.biojava.nbio.core.sequence.ProteinSequence; import org.biojava.nbio.core.sequence.compound.AminoAcidCompound; import org.biojava.nbio.core.sequence.compound.AminoAcidCompoundSet; import org.biojava.nbio.core.sequence.io.FastaReader; import org.biojava.nbio.core.sequence.io.FastaReaderHelper; import org.biojava.nbio.core.sequence.io.GenericFastaHeaderParser; import org.biojava.nbio.core.sequence.io.ProteinSequenceCreator;
 
 public class main {
 
-    public static void main(String[] args) throws CompoundNotFoundException {
-        DNASequence seq = new DNASequence("GTAC");
+    public static void main(String[] args) throws Exception{
+       /*
+        * Method 1: With the FastaReaderHelper
+        */
+        //Try with the FastaReaderHelper
+        LinkedHashMap<String, ProteinSequence> a = FastaReaderHelper.readFastaProteinSequence(new File("/home/marc/IdeaProjects/DynamiteAndButterflies/out.fa"));
+        //FastaReaderHelper.readFastaDNASequence for DNA sequences
+
+        for (  Entry<String, ProteinSequence> entry : a.entrySet() ) {
+            System.out.println( entry.getValue().getOriginalHeader() + "=" + entry.getValue().getSequenceAsString() );
+        }
     }
+
 }
