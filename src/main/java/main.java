2b04f2be32573c032c0ccabf5ec1a560a8faba65
Marc Visser <M.Visser-6@student.tudelft.nl>
2017-04-25 11:45:17 +0200

    added maven dependency for BioJava

---
+++ src/main/java/main.java
@@ -0,0 +1,14 @@
+import org.biojava.nbio.core.exceptions.CompoundNotFoundException;
+import org.biojava.nbio.core.sequence.DNASequence;
+
+/**
+ * Created by marc on 25-4-17.
+ */
+
+
+public class main {
+
+    public static void main(String[] args) throws CompoundNotFoundException {
+        DNASequence seq = new DNASequence("GTAC");
+    }
+}
