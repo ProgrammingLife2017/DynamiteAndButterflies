79531c12eea8296ed78190d04d9fb63ac706d742
BugOrFeature <marceyv@gmail.com>
2017-05-04 23:23:45 +0200

    removed unused dependencies

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,3 @@
-import java.io.File; import java.io.FileInputStream; import java.util.LinkedHashMap; import java.util.Map.Entry;
-import org.biojava.nbio.core.sequence.ProteinSequence; import org.biojava.nbio.core.sequence.compound.AminoAcidCompound; import org.biojava.nbio.core.sequence.compound.AminoAcidCompoundSet; import org.biojava.nbio.core.sequence.io.FastaReader; import org.biojava.nbio.core.sequence.io.FastaReaderHelper; import org.biojava.nbio.core.sequence.io.GenericFastaHeaderParser; import org.biojava.nbio.core.sequence.io.ProteinSequenceCreator;
-
 public class main {
 
     public static void main(String[] args) throws Exception{
