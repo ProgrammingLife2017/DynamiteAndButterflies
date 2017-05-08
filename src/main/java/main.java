10d4f62b65600194365f114dcb19305cf6ea6771
Jipje <jacrietveld@student.tudelft.nl>
2017-05-08 10:59:07 +0200

    Adjusted the main so that it runs

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,14 +1,8 @@
-import graph.SequenceGraph;
-import parser.GfaParser;
+import GUI.App;
 
 public class main {
 
     public static void main(String[] args) throws Exception{
-        GfaParser parser = new GfaParser();
-        SequenceGraph graph = parser.parse("src/main/resources/test (1).gfa");
-        graph.initialize();
-        graph.layerizeGraph();
-        System.out.println("Done");
+        App.main(args);
     }
-
 }
