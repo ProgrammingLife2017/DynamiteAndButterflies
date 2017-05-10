ebf5a807f8551c4bdb401bdad696537e9a5236d1
Jipje <jacrietveld@student.tudelft.nl>
2017-05-10 13:40:13 +0200

    Added a function in the GraphDrawer to draw only a part of the graph

--- src/main/java/GUI/GraphDrawer.java
+++ src/main/java/GUI/GraphDrawer.java
@@ -67,6 +67,26 @@ public class GraphDrawer {
         }
     }
 
+    public void drawShapes(int startColumn, int endColumn) {
+        gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+        ArrayList<ArrayList<Node>> columns = graph.getColumnList();
+        gc.setFill(Color.BLUE);
+        int counter = 0;
+        int lastColumn = Math.min(endColumn, columns.size());
+        for (int j = startColumn; j < lastColumn; j++) {
+            counter++;
+            ArrayList<Node> column = columns.get(j);
+            for (int i = 0; i < column.size(); i++) {
+                if (column.get(i) instanceof DummyNode) {
+                    gc.setFill(Color.BLACK);
+                }
+                //gc.fillRoundRect((j * (xSize + EDGE_LENGTH)) + 50, Y_BASE + (i * 50), xSize, Y_SIZE, 10, 10);
+                gc.fillRoundRect(counter * (((gc.getCanvas().getWidth() - 20) / zoomLevel) + xSize), Y_BASE + (i * 50), xSize, Y_SIZE, 10, 10);
+                gc.setFill(Color.BLUE);
+            }
+        }
+    }
+
     public void reShape(double xDifference) {
         gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
         ArrayList<ArrayList<Node>> columns = graph.getColumnList();
