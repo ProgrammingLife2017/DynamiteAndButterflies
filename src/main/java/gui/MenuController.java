5e66713f54c1762491263c32cb766688179fae86
lex_b <lex_boleij@live.nl>
2017-05-29 16:57:16 +0200

    Oops, left something commented, that should not have been.

--- src/main/java/gui/MenuController.java
+++ src/main/java/gui/MenuController.java
@@ -97,8 +97,8 @@ public class MenuController implements Observer {
 
         recentController.initialize(prefs);
         ps = new PrintStream(new Console(consoleArea));
-        //System.setErr(ps);
-        //System.setOut(ps);
+        System.setErr(ps);
+        System.setOut(ps);
     }
 
     /**
