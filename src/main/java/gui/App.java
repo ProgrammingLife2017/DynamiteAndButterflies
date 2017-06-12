77218725190383953e5136d5e4c2f037f6296fe5
Jipje <J.A.C.Rietveld@student.tudelft.nl>
2017-06-12 12:17:08 +0200

    Does this solve the mac issue?

--- src/main/java/gui/App.java
+++ src/main/java/gui/App.java
@@ -65,8 +65,8 @@ public class App extends Application {
             stage.setTitle("Wow!! DynamiteAndButterflies genome visualiser\t");
             stage.setScene(scene);
             stage.setResizable(true);
-            stage.setMaximized(true);
             stage.show();
+            stage.setMaximized(true);
             System.out.println(path + " loaded on the stage");
             return loader;
         } catch (IOException e) {
